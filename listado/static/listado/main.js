586490b06126c4cd41c2d08e69d9f00855c6b411
Angel Lars <lars505@gmail.com>
2024-04-24 16:11:54 -0600

    estilos de la lista se termino el estilo de las lista de tareas :)

--- listado/static/listado/main.js
+++ listado/static/listado/main.js
@@ -19,14 +19,38 @@ function cargarLista(){
 
             const li = document.createElement('li')
             const div = document.createElement('div')
+            const div2 = document.createElement('div')
             const span = document.createElement('span')
+            const a = document.createElement('a')
+            const a2 = document.createElement('a')
+            const span2 = document.createElement('span')
+            const span3 = document.createElement('span')
 
             li.classList.add("list-group-item")
             li.classList.add("d-flex")
+            li.classList.add("justify-content-between")
             li.classList.add("mb-2")
 
             div.classList.add("d-flex")
             div.classList.add("flex-column")
+
+            div2.classList.add("iconos")
+            div2.classList.add("d-flex")
+            div2.classList.add("justify-content-center")
+            div2.classList.add("align-items-center")
+            
+            a.classList.add("d-flex")
+            a.setAttribute('href', "{% url 'realizada' task.id %}")
+            a2.classList.add("d-flex")
+            a2.setAttribute('href', "{% url 'eliminar' task.pk %}")
+            
+            span2.classList.add("material-symbols-outlined")
+            span2.classList.add("done")
+            span2.innerHTML = "done"
+            
+            span3.classList.add("material-symbols-outlined")
+            span3.classList.add("trash")
+            span3.innerHTML = "delete"
             
             span.classList.add("form-text")
 
@@ -34,7 +58,12 @@ function cargarLista(){
             span.innerHTML = task.descripcion;
 
             li.append(div)
-            li.append(span)
+            div.append(span)
+            a.append(span2)
+            a2.append(span3)
+            div2.append(a)
+            div2.append(a2)
+            li.append(div2)
             
 
             document.querySelector("#ul-tasks").append(li);
