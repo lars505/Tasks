8370c243752eedf3d9bc808f8665472cbb7f387c
Angel Larios <lars50593@gmail.com>
2024-05-20 19:21:14 -0600

    se actualizo settings.py

--- tareas/settings.py
+++ tareas/settings.py
@@ -90,7 +90,6 @@ DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
         default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
     )
 }
 
